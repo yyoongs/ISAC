94bcf03cccf61170d364477a46ef41eb77f5a835
gungadinn <ch-aa@naver.com>
2019-08-19 18:12:06 +0900

    update file

--- homepage/app.py
+++ homepage/app.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/result')
+@app.route('/result',methods = ['POST', 'GET'])
 def result():
-    return render_template('resultpage.html')
+    if request.method == 'POST':
+        title = request.form.get('inputTitle')
+        big = request.form.get('SelectBigCate')
+        mid =request.form.get('SelectMidCate')
+        small = request.form.get('SelectSmallCate')
+        situation = request.form.get('SelectSituation')
+        content = request.form.get('inputContent')
+        return render_template('result.html',  title=title, big=big, mid=mid, small=small, situation=situation, content=content)
+#        return(str(big+' '+mid+' '+small))
+#        return render_template("result.html", result=result)
+ #   return render_template('result.html')
 
 
 @app.route('/custom')
@@ -27,6 +36,9 @@ def relation():
 def rightnow():
     return render_template('rightnow.html')
 
+@app.route('/solution')
+def solution():
+    return render_template('solution.html')
 
 if __name__ == '__main__':
     app.run()
