ed72958c80ab0debf6f54f92b96c54826b37aca7
gungadinn <ch-aa@naver.com>
2019-08-21 05:37:01 +0900

    update dao

--- homepage/dao/counselingdao.py
+++ homepage/dao/counselingdao.py
@@ -1,6 +1,4 @@
 # -- coding: utf-8 --
-#처리된 데이터를 json형식으로 리턴해주기위해 사용
-import json
 #python mysql 연결 드라이버
 import pymysql
 import time
@@ -19,13 +17,10 @@ def sql_template(type, sql, params=None):
         if type == 3 :
             with connetion.cursor() as cursor :
                 # 데이터 입력
-                rows = cursor.execute(sql, params)
+                cursor.execute(sql, params)
+
                 # commit
                 connetion.commit()
-                # sql="select id, title from realtime_counsel where title=%s"
-                # print(params[0])
-                # cursor.execute(sql, (params[0],))
-                # return cursor.fetchone()
 
         else :
             # 1 = fetchall() 2 = fetchone()
@@ -42,17 +37,18 @@ def sql_template(type, sql, params=None):
         connetion.close()
 
 def getCounseling(title):
-    sql = "select title, big_cate, mid_cate, situation, question from realtime_counsel where title=%s"
+    sql = "select title, big_cate, mid_cate, question_date, question from realtime_counsel where title=%s"
 
     return sql_template(1, sql, (title,))
 
-#넘어온 schedule을 등록
+#넘어온 상담들 등록
 def setCounseling(counsel):
     ts = time.time()
     timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
     #넘어온 데이터중 빈값이 있으면 0 리턴
     # 처리하기
 
-    sql = "INSERT INTO realtime_counsel (title, big_cate, mid_cate, small_cate, situation, question_date, question) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    params = (counsel['title'], counsel['big'], counsel['mid'],counsel['small'], counsel['situation'], timestamp, counsel['question'])
+    sql = "INSERT INTO realtime_counsel (title, big_cate, mid_cate, small_cate, question_date, question) VALUES (%s, %s, %s, %s, %s, %s)"
+    params = (counsel['title'], counsel['big'], counsel['mid'],counsel['small'],  timestamp, counsel['question'])
     return sql_template(3, sql, params)
