26f0409c6b8d872a7b0b7f97d34bdbaec970f29d
gungadinn <ch-aa@naver.com>
2019-08-23 21:23:17 +0900

    update for predict

--- homepage/myModule/counselController.py
+++ homepage/myModule/counselController.py
@@ -64,19 +64,35 @@ class CounselController():
     # 모델을 통한 예측
     def predictModel(self):
         big = request.form.get('SelectBigCate')
-        model = load_model(os.path.join(APP_STATIC, 'predictModel/'+big+'_add_paper.h5'))
-        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+        addPaperModel = load_model(os.path.join(APP_STATIC, 'predictModel/'+big+'_add_paper.h5'))
+        addPaperModel.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+        goojaeModel = load_model(os.path.join(APP_STATIC, 'predictModel/' + big + '_goojae.h5'))
+        goojaeModel.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+        jojungModel = load_model(os.path.join(APP_STATIC, 'predictModel/'+big+'_jojung.h5'))
+        jojungModel.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+        negoableModel = load_model(os.path.join(APP_STATIC, 'predictModel/'+big+'_nego_able.h5'))
+        negoableModel.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
 
         with open(os.path.join(APP_STATIC, 'predictModel/tag_list.json')) as f:
             List = json.load(f)
         a = ['환불', '디조', '요금', 'as']
         tt = pd.DataFrame([[1 if _ in a else 0 for _ in List[big]]])
-        print(model.predict(tt))
-        pass
+        print("추가서류 여부 예측")
+        print(addPaperModel.predict(tt))
+        print("구제 예측")
+        print(goojaeModel.predict(tt))
+        print("조정 예측")
+        print(jojungModel.predict(tt))
+        print("협상 예측")
+        print(negoableModel.predict(tt))
 
 
     def mobum_with_test(self):
-        print('mobum')
         model = md.test_model(title, doc_list, data_recp_num, result2)
         model.weight_comp('파손 배송 제품 구매', weight=0.35, top=5)
 
